7a0ca8f1933eaa23e4110a2343ce6bf0f1854d2a
Excited-Electron18 <excited.electron18@gmail.com>
2023-07-05 15:37:52 +0530

    Update NSE_Read.py

--- Python/Trading/NSE_Read.py
+++ Python/Trading/NSE_Read.py
@@ -1,21 +1,25 @@
-import numpy as np
-import pandas as pd
-import requests
-import xlsxwriter
-import math
-import yfinance as yf
-import plotly.graph_objs as go
-import matplotlib.pyplot as plt
+##import numpy as np
+##import pandas as pd
+##import requests
+##import xlsxwriter
+##import math
+##import yfinance as yf
+##import plotly.graph_objs as go
+##import matplotlib.pyplot as plt
 from nsetools import Nse
-from nsepython import *
+##from nsepython import *
 
 print("Import Done")
 
 nse = Nse()
 print("Data Request Start")
-data = nse.get_quote("TCS")
-print(data)
+if(nse.is_valid_code("TCS")):
+    data = nse.get_quote("TCS")
+    print(data)
+else:
+    print("Invalid Code")
 
+nse.get_stock_codes()
 ##print(data['lastPrice'])
 ##print("previousClose : ",nse_eq("TCS")['priceInfo']['previousClose'])
 ##print("open : ",nse_eq("TCS")['priceInfo']['open'])
